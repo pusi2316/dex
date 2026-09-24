ff094df7731035d3b67b1a5739495be3140187c4
Bence Puschmann <puschmann2@gmail.com>
2026-09-23 20:19:34 -0700

    chore: comment out unused search_obsidian_vault import in mod.rs

--- dex-rust/src-tauri/src/brain/mod.rs
+++ dex-rust/src-tauri/src/brain/mod.rs
@@ -3,7 +3,7 @@ pub mod claude_code_search;
 pub mod router;
 
 use claude_api::ask_claude_api;
-use claude_code_search::search_obsidian_vault;
+//use claude_code_search::search_obsidian_vault;
 use router::should_search_vault;
 
 /*#[tauri::command]
